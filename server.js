d3f60133a163071e51b556d3c1b52ec3d5e66e51
Saydeezr <saydeezr@gmail.com>
2024-07-05 15:40:10 -0400

    started setup

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const routes = require('./routes');
+
+const { MongoClient } = require('mongodb');
+const url = process.env.MONGOURL;
+const client = new MongoClient(url);
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
