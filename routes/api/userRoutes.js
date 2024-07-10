940991008b813aa2fa68a7dfa1fe7bb48a2a3d65
Saydeezr <saydeezr@gmail.com>
2024-07-10 11:26:52 -0400

    added routes to post/delete friends

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -66,4 +66,39 @@ router.delete('/:username', async (req,res) =>{
     }
 })
 
+//add friend to a User
+router.post('/:username/friends/:friendId', async (req,res) => {
+    try {                                
+        const user = await User.findOne({username: req.params.username})
+        const friend = await User.findById(req.params.friendId)
+
+        if(user && friend) {
+            user.friends.push(friend._id);
+            await user.save();
+            res.status(200).json({message: `Friend successfully added to ${user}`});
+        } else { 
+            res.status(500).json({ message: 'User not found'});
+        } 
+    } catch(err){
+        return res.status(500).json(err);
+}});
+
+//delete friend
+router.delete('/:username/friends/:friendId', async (req,res) => {
+    try {                                
+        const user = await User.findOne({username: req.params.username})
+        const friend = await User.findById(req.params.friendId)
+
+        if(user && friend) {
+            user.friends.pop(friend._id);
+            await user.save();
+            res.status(200).json({message: `Friend successfully deleted`});
+        } else { 
+            res.status(500).json({ message: 'User not found'});
+        } 
+    } catch(err){
+        return res.status(500).json(err);
+}});
+
+
 module.exports = router;
